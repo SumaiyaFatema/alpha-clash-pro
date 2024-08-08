2ab220039ac9eb561c9d5d3f0a1d7c794ffacc19
SumaiyaFatema <suma540iya@gmail.com>
2024-08-08 19:04:02 +0600

    added alphabet element

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,34 @@
+// function play(){
+//     //step-1 hide the home screen. to hide the screen add the class hidden to the home section.
+
+//     const homeSection = document.getElementById('home-screen');
+//     //console.log(homeSection.classList);
+//     homeSection.classList.add('hidden');
+//     //show the playground
+
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden')
+
+
+// }
+
+function continueGame(){
+    //step-1 generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your random Alphabet',alphabet)
+
+    //set randomly generated alphabet to the screen(show it )
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    //set background color
+
+    setBackgroundColorById(alphabet);
+
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
